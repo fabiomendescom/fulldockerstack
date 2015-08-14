5200c62d8d990d4ec7c98054a206795d4344b522
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2015-08-14 01:30:37 +0000

    does not work on vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Install docker
-wget -qO- https://get.docker.com/ | sh
+wget -qO- https://get.docker.com/ | sed -r 's/^apt-get install -y lxc-docker$/apt-get install lxc-docker-1.6.2/g' | sh
 wget -O /home/vagrant/zookeepercli_1.0.10_amd64.deb https://github.com/outbrain/zookeepercli/releases/download/v1.0.10/zookeepercli_1.0.10_amd64.deb 
 sudo dpkg -i /home/vagrant/zookeepercli_1.0.10_amd64.deb
 sudo apt-get install -f
-#sudo echo "export ZOOKPRSVRS=$(ifconfig docker0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'):2181" >> /etc/profile
-#source /etc/profile
 
-# Start all containers
-cd /vagrant
-./startall.sh
 
